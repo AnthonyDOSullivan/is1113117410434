ee75a818144819990769a21cdbe2a553365dba1a
AnthonyDOSullivan <117410434@umail.ucc.ie>
2018-02-23 11:23:27 +0000

    line break php

--- ebusiness/Ebus3.php
+++ ebusiness/Ebus3.php
@@ -12,7 +12,9 @@ session_start();
         <?php
         // Echo session variables that were set on previous page
         echo "Name: " . $_SESSION["user_name"];
+        nl2br();
         echo "E-mail" . $_SESSION["user_email"];
+        nl2br();
         echo "Total is " . $_SESSION["total"] . ".";
         ?>
     </body>
